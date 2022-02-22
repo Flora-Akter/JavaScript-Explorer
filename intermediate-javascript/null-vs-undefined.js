2d564d14ae5e242c5895965cd81916e117917096
Flora-Akter <soniaflora124@gmail.com>
2022-02-22 09:33:16 +0600

    null vs undefined cleared

---
+++ intermediate-javascript/null-vs-undefined.js
@@ -0,0 +1,26 @@
+
+let name; 
+console.log(name);
+_____________________________
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+const result = add(13,82);
+console.log(result);
+____________________________
+function add(num1,num2){
+    console.log(num1 , num2)
+}
+const result = add(13);
+console.log(result);
+______________________________________
+const friends = {age: 17, phone:2891}
+console.log(friends.salary);
+
+_______________________
+let fun = undefined;
+console.log(fun);
+___________________________
+let ages = [21,2,78,67];
+console.log(ages[11]);
