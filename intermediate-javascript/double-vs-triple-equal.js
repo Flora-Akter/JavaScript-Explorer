63a7c521ec135d94d223fbeac219652e2795c565
Flora-Akter <soniaflora124@gmail.com>
2022-02-22 10:30:49 +0600

    double-equeal vs triple-equal cleared

---
+++ intermediate-javascript/double-vs-triple-equal.js
@@ -0,0 +1,36 @@
+const first = 2;
+const second = 2;
+if( first == second){
+    console.log("condition is true");
+} 
+else{
+    console.log("condition is false");
+}
+//  _____________________________________
+const first1 = 2;
+const second1 = "2";
+if( first1 === second1){
+    console.log("condition is true");
+} 
+else{
+    console.log("condition is false");
+}
+// _______________________________________
+const first2 = 0;
+const second2 = false;
+if( first2 == second2){
+    console.log("condition is true");
+} 
+else{
+    console.log("condition is false");
+}
+
+// _________________________________________
+const first3 = 0;
+const second3 = false;
+if( first3 === second3){
+    console.log("condition is true");
+} 
+else{
+    console.log("condition is false");
+}
