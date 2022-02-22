ae9db97d2cc86f4502fd8482fa0979023221627e
Flora-Akter <soniaflora124@gmail.com>
2022-02-22 17:39:37 +0600

    global-local scope

---
+++ intermediate-javascript/scope.js
@@ -0,0 +1,23 @@
+ bonus = 20;
+function sum(first, second){
+    let result = first + second + bonus;
+    if(result > 9){
+        const mood = "happy";
+        var mood2= "cranky";
+        mood= "fishy";
+        mood= "funky";
+        console.log(mood);
+    }
+    // console.log(mood);
+    console.log(mood2);
+    // console.log(bonus);
+    return result;
+}
+
+const output = sum(3,7);
+console.log(output);
+// console.log(bonus);
+
+console.log(day);
+var day = "friday";
+// console.log(day);
