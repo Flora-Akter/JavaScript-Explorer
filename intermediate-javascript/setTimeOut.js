38d6c37fa38835783489899820de7b5fa174828f
Flora-Akter <soniaflora124@gmail.com>
2022-02-24 20:37:42 +0600

    setTimeOut and setInterval done

---
+++ intermediate-javascript/setTimeOut.js
@@ -0,0 +1,12 @@
+console.log(2221);
+// setTimeout ekbar excute hobe jsomoy dea hobe setar por
+setTimeout(() =>{
+    console.log(2222)
+},0);
+
+// setInterval nirthristho somoy por por excute hote thakbe unlimited
+setInterval(() => {
+    console.log("lazy me")
+}, 1000)
+console.log(2223);
+console.log(2224);
